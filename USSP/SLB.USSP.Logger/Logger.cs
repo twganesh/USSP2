19886466974b196ece60d6c2d62c28afd2f05348
twganesh <ganesh.tiwari@capgemini.com>
2016-11-17 20:09:21 +0530

    Initial Commit

---
+++ USSP/SLB.USSP.Logger/Logger.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SLB.USSP.Logger
+{
+    public class Logger
+    {
+    }
+}
